7ccfa9634e084369649200401fe307c1d569b40c
rebeckalscarlsson <rebecka.ls.carlsson@gmail.com>
2019-02-01 11:09:29 +0100

    Snyggat till lite

--- wwwroot/js/main.js
+++ wwwroot/js/main.js
@@ -1,4 +1,6 @@
 ﻿getAllObservations();
+let allObservations = "";
+let species = "";
 
 async function getAllSpecies() {
 
@@ -6,19 +8,23 @@ async function getAllSpecies() {
 
     if (response.status === 200) {
 
-        let species = await response.json();
+        species = await response.json();
         console.log("species", species)
 
-        let html = "<table>"
-
-        for (let s of species) {
-            html += `<tr><td>${s}</td></tr>`;
-        }
-        document.getElementById("allSpecies").innerHTML = html + `</table>`;
+        printAllSpecies();
 
     } else {
         console.log('Something went wrong..')
     }
+}
+
+function printAllSpecies() {
+    let html = "<table>"
+
+    for (let s of species) {
+        html += `<tr><td>${s}</td></tr>`;
+    }
+    document.getElementById("allSpecies").innerHTML = html + `</table>`;
 }
 
 async function getAllObservations() {
@@ -26,26 +32,30 @@ async function getAllObservations() {
     let response = await fetch(`/observation/`, { method: "GET" });
 
     if (response.status === 200) {
-        let allObservations = await response.json();
+        allObservations = await response.json();
         console.log("allObservations", allObservations)
 
-        let html = "<table><tr><th>Date</th><th>Species</th><th>Location</th><th>Notes</th></tr>";
-
-        for (let o of allObservations) {
-            html += `<tr>
-                        <td>${formatDate(o.date)}</td>
-                        <td>${o.species}</td>
-                        <td>${o.location === null ? "" : o.location}</td>
-                        <td>${o.notes === null ? "" : o.notes}</td>
-                    </tr>`;
-        }
-
-        document.getElementById("observations").innerHTML = html + `</table>`;
+        printAllObservations();
         getAllSpecies();
 
     } else {
         console.log('Something went wrong..');
     }
+}
+
+function printAllObservations() {
+    let html = "<table><tr><th>Date</th><th>Species</th><th>Location</th><th>Notes</th></tr>";
+
+    for (let o of allObservations) {
+        html += `<tr>
+                        <td>${formatDate(o.date)}</td>
+                        <td>${o.species}</td>
+                        <td>${o.location === null ? "" : o.location}</td>
+                        <td>${o.notes === null ? "" : o.notes}</td>
+                    </tr>`;
+    }
+
+    document.getElementById("observations").innerHTML = html + `</table>`;
 }
 
 function formatDate(date) {
